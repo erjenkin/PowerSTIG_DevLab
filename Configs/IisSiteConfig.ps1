2fc573d973334cf7bb5d1085e5d12772cd75923b
Eric Jenkins <erjenkin@microsoft.com>
2019-05-02 11:02:54 -0400

    Create IisSiteConfig.ps1

---
+++ Configs/IisSiteConfig.ps1
@@ -0,0 +1,23 @@
+Configuration IisSite_Config
+{
+    param
+    (
+        [parameter()]
+        [string]
+        $NodeName = 'localhost'
+    )
+
+     Import-DscResource -ModuleName PowerStig 
+
+     Node $NodeName
+    {
+        IisSite ServerConfiguration
+        {
+            IisVersion  = '8.5'
+            WebSiteName = 'Default Web Site'
+
+         }
+    }
+}
+
+IisSite_Config
