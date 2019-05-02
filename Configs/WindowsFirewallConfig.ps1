21fe2a96856683099e91075098674816c1aba732
Eric Jenkins <erjenkin@microsoft.com>
2019-05-02 10:55:29 -0400

    Update WindowsFirewallConfig.ps1

--- Configs/WindowsFirewallConfig.ps1
+++ Configs/WindowsFirewallConfig.ps1
@@ -7,7 +7,7 @@ Configuration WindowsFirewall_config
         $NodeName = 'localhost'
     )
 
-    Import-DscResource -ModuleName PowerStig -ModuleVersion 3.2.0
+    Import-DscResource -ModuleName PowerStig
 
     Node $NodeName
     {
